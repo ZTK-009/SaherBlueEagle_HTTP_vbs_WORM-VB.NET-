da33129c8e05a56b6659d7dfd6a257604fcb5006
SaherBlueEagle <44013704+SaherBlueEagle@users.noreply.github.com>
2019-04-11 11:30:59 +0200

    Add files via upload

---
+++ Saher_Blue_Eagle_WORM_Open_soruce/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Saher_Blue_Eagle_WORM")> 
+<Assembly: AssemblyDescription("Saher_Blue_Eagle_WORM")> 
+<Assembly: AssemblyCompany("Saher_Blue_Eagle_WORM")> 
+<Assembly: AssemblyProduct("Saher_Blue_Eagle_WORM")> 
+<Assembly: AssemblyCopyright("Copyright © Saher_Blue_Eagle_WORM 2019")> 
+<Assembly: AssemblyTrademark("Saher_Blue_Eagle_WORM")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4ece2bdf-5790-477a-a68b-02b72e704710")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
